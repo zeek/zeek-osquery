68a5f1c6808460c982c1a53fc990e3f6bdc74a86
Robin Sommer <robin@icir.org>
2016-02-01 15:46:00 -0800

    Adding configure option to set path to osquery's extension socket.
    This is to switch the deployment model to running osqueryd and
    extension separately (i.e., without autoloading extensions).

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-/* 
+/*
  *  Copyright (c) 2015, Next Generation Intelligent Networks (nextGIN), RC.
  *  Institute of Space Technology
  *  All rights reserved.
- * 
+ *
  *  This source code is licensed under the BSD-style license found in the
  *  LICENSE file in the root directory of this source tree. An additional grant
  *  of patent rights can be found in the PATENTS file in the same directory.
@@ -25,25 +25,30 @@
 #include "utility.h"
 #include "StateMachine.h"
 
+#include "config.h"
 
+namespace osquery  {
+    DECLARE_string(extensions_socket);
+}
 
-// :osquery::REGISTER_EXTERNAL to define BrokerQueryManagerPlugin 
+// :osquery::REGISTER_EXTERNAL to define BrokerQueryManagerPlugin
 REGISTER_EXTERNAL(BrokerQueryManagerPlugin, "config", "brokerQueryManager")
-
 
 
 // main runner
 int main(int argc, char* argv[]) {
-    
+
+  FLAGS_extensions_socket = OSQUERY_SOCKET;
+
   //osquery::runner start logging, threads, etc. for our extension
   osquery::Initializer runner(argc, argv, OSQUERY_EXTENSION);
   LOG(WARNING) <<"Initialized OSquery." ;
   //wait 1sec for osqueryd to load
   usleep(1000000);
-  
+
     //SignalHandler object to trace kill signal
   SignalHandler *signalHandler = new SignalHandler;
-  
+
   try
     {
       // try setting up signal handler for kill signal
@@ -55,22 +60,22 @@ int main(int argc, char* argv[]) {
 
       if(statusCode == SUCCESS)
       {
-        // delete SignalHandler object 
+        // delete SignalHandler object
         delete signalHandler;
       }
     }
   // catches exception thrown at kill signal setup time
     catch(SignalException& e)
     {
-        // delete SignalHandler object 
+        // delete SignalHandler object
         delete signalHandler;
         LOG(ERROR) << "SignalException: " <<e.what();
     }
- 
-    
+
+
 LOG(WARNING) <<"Shutting down extension";
 // Finally shutdown.
 runner.shutdown();
-              
+
 return 0;
 }
